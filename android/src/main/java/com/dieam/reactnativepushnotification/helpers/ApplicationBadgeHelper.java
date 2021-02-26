bb64cb5f52dc92c81340a06afdccf06f041b6f5d
Marcos Willian <markusuuuu@gmail.com>
2021-02-26 14:01:58 -0300

    test java code for xiaomi device.

--- android/src/main/java/com/dieam/reactnativepushnotification/helpers/ApplicationBadgeHelper.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/helpers/ApplicationBadgeHelper.java
@@ -2,9 +2,15 @@ package com.dieam.reactnativepushnotification.helpers;
 
 import android.content.ComponentName;
 import android.content.Context;
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.app.IntentService;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.app.NotificationChannel;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.os.Build;
 
 import com.facebook.common.logging.FLog;
 
@@ -15,16 +21,23 @@ import me.leolin.shortcutbadger.ShortcutBadger;
  * Helper for setting application launcher icon badge counts.
  * This is a wrapper around {@link ShortcutBadger}:
  */
-public class ApplicationBadgeHelper {
+public class ApplicationBadgeHelper extends IntentService {
 
     public static final ApplicationBadgeHelper INSTANCE = new ApplicationBadgeHelper();
 
     private static final String LOG_TAG = "ApplicationBadgeHelper";
 
+    private static final String NOTIFICATION_CHANNEL = "com.dieam.reactnativepushnotification.helpers";
+
+    private int notificationId = 0;
+
     private Boolean applyAutomaticBadger;
     private ComponentName componentName;
 
+    private NotificationManager mNotificationManager;
+
     private ApplicationBadgeHelper() {
+        super("ApplicationBadgeHelper");
     }
 
     public void setApplicationIconBadgeNumber(Context context, int number) {
@@ -32,6 +45,41 @@ public class ApplicationBadgeHelper {
             componentName = context.getPackageManager().getLaunchIntentForPackage(context.getPackageName()).getComponent();
         }
         tryAutomaticBadge(context, number);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        super.onStart(intent, startId);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            int badgeCount = intent.getIntExtra("badgeCount", 0);
+
+            mNotificationManager.cancel(notificationId);
+            notificationId++;
+
+            Notification.Builder builder = new Notification.Builder(getApplicationContext())
+                    .setContentTitle("")
+                    .setContentText("");
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                setupNotificationChannel();
+
+                builder.setChannelId(NOTIFICATION_CHANNEL);
+            }
+
+            Notification notification = builder.build();
+            ShortcutBadger.applyNotification(getApplicationContext(), notification, badgeCount);
+            mNotificationManager.notify(notificationId, notification);
+        }
     }
 
     private void tryAutomaticBadge(Context context, int number) {
@@ -47,5 +95,13 @@ public class ApplicationBadgeHelper {
             return;
         }
         ShortcutBadger.applyCount(context, number);
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    private void setupNotificationChannel() {
+        NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANNEL, "ShortcutBadger Sample",
+                NotificationManager.IMPORTANCE_DEFAULT);
+
+        mNotificationManager.createNotificationChannel(channel);
     }
 }
